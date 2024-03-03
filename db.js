2bd6e525779be8f20df304e59b0361ae57e00b38
MOHAMMED ABRAR SHAREEF <141035990+zohaibabrar01@users.noreply.github.com>
2024-03-03 16:02:28 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
-// db.js
 import pkg from 'pg';
 const { Pool } = pkg;
 
+// Replace these values with your Render PostgreSQL database credentials
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'CUSTOMER_INFO',
-  password: '@ALLAh786',
+  user: 'zithara_user',
+  host: 'dpg-cni4do6n7f5s73clfpq0-a.oregon-postgres.render.com',
+  database: 'zithara', // or your database name
+  password: 'XMZGK5Mwf8GdXwwTdz0nGSgew21xcdkn',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false // For development purposes only, remove in production
+  }
 });
 
 export { pool }; // Export pool as a named export
